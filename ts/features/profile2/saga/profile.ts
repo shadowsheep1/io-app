2da50e25e18e95ae8ed3b6d0b9900da932a9e536
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-12 22:17:41 +0100

    Remove console.log (I need a commit with them as a reminder).

--- ts/features/profile2/saga/profile.ts
+++ ts/features/profile2/saga/profile.ts
@@ -47,7 +47,6 @@ export function* refreshProfile(): Generator<ReduxSagaEffect, void, any> {
   );
 
   // Load the profile info
-  console.log("1. Call API");
   const maybeUserProfile: SagaCallReturnType<typeof loadProfile> = yield* call(
     loadProfile,
     backendClient.getProfile
@@ -55,10 +54,9 @@ export function* refreshProfile(): Generator<ReduxSagaEffect, void, any> {
 
   if (O.isNone(maybeUserProfile)) {
     /**
-     * If we didn't succeed, 
+     * If we didn't succeed,
      * we try again after waiting some time.
      */
-    console.log("3. Retrying...");
     yield* delay(BACKEND_PROFILE_RETRY_DELAY_INTERVAL);
     yield* put(initializeProfileRequest());
   }
@@ -83,16 +81,14 @@ export function* loadProfile(
            * different from the ones of our return types.
            * Here for example we manage only:
            * - 200
-           * - 400  
-           * - 401 
-           * - 429 
-           * - 500 
+           * - 400
+           * - 401
+           * - 429
+           * - 500
            */
-          console.log(`2.💥 Error ${readablePrivacyReport(reason)}`);
           throw Error(readablePrivacyReport(reason));
         },
         response => {
-          console.log("🙈 Hi, there! Here I am!!!");
           if (response.status === 200) {
             return O.some(response.value);
           }
@@ -100,28 +96,23 @@ export function* loadProfile(
             return O.none;
           }
           /**
-           * Here we come for all other "known" status code: 400, 500. 
+           * Here we come for all other "known" status code: 400, 500.
            * 429 is known, but it seems to be managed differently (by the API client).
            * For example, having a Server Error 500,
            * but the JSON derserializer expect a JSON Body.
            */
-          console.log("2.2 status code != 200 | 401");
           throw response
             ? Error(`response status ${response.status}`)
             : Error(I18n.t("profile.errors.load"));
         }
       )
     );
-    console.log("2.1 Check result");
     yield* checkBackendProfile(backendProfile);
     return backendProfile;
   } catch (e) {
     if (e === "max-retries") {
       // This comment is useful to be aware of this king of exception.
       // Only for my onboarding purpose [-;
-      console.log("max-retries");
-    } else {
-      console.log(`4. Outer exception ${e}`);
     }
     yield* put(profileLoadFailure(convertUnknownToError(e)));
   }
@@ -130,10 +121,8 @@ export function* loadProfile(
 
 function* checkBackendProfile(profile: O.Option<InitializedProfile>) {
   if (O.isSome(profile)) {
-    console.log(`2.1.1 Success 🚀 ${profile.value}`);
     yield* put(profileLoadSuccess(profile.value));
   } else {
-    console.log("2.1.1 Expired ⏰");
     yield* put(sessionExpired());
   }
 }
